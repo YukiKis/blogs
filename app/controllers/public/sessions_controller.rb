4b7047dea2213c9fc3910e22e833fa30040b95c4
EC2 Default User <ec2-user@ip-172-31-42-86.ec2.internal>
2021-01-25 01:47:10 +0000

    finish login page

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,22 @@
 class Public::SessionsController < ApplicationController
   def new
+    if session[:user]
+      redirect_to user_path(session[:user]), notice: "既にログインしています。"
+    end
   end
   
   def create
+    if session[:user]
+      redirect_to user_path, notice: "既にログインしています。"
+    else
+      user = User.find_by(name: params[:name])
+      if user && user.authenticate(params[:password])
+        session[:user] = user.id
+        redirect_to user, notice: "ログインしました"
+      else
+        render :new
+      end
+    end
   end
   
   def destroy
--- spec/systems/public/sessions_spec.rb
+++ spec/systems/public/sessions_spec.rb
@@ -2,38 +2,25 @@ require "rails_helper"
 
 RSpec.describe "SessionsController", type: :system do
   context "on new" do
-    before do
-      visit new_user_path
-    end
-    it "is right URL" do
-      expect(current_path).to eq new_user_path
-    end
-    it "has name form" do
+    let(:user1){ create(:user1) }
+    it "has name field" do
       expect(page).to have_field "user[name]"
     end
-    it "has password field" do
+    it "has password_field" do
       expect(page).to have_field "user[password]"
     end
-    it "has password_confirmation field" do
-      expect(page).to have_field "user[password_confirmation]"
+    it "has button to login" do
+      expect(page).to have_button "ログイン"
     end
-    it "has agreement checkbox" do
-      expect(page).to have_field "user[agreement]"
-    end
-    it "has button to submit" do
-      expect(page).to have_button "登録"
-    end
-    it "successfully create" do
+    it "successfully login" do
       fill_in "user[name]", with: "yuki"
       fill_in "user[password]", with: "testtest"
-      fill_in "user[password_confirmation]", with: "testtest"
-      check "OK"
-      click_button "登録"
-      expect(page).to have_content "登録出来ました"
+      click_button "ログイン"
+      expect(page).to have_content "ログインしました"
     end
-    it "fails to create" do
-      click_button "登録"
-      expect(page).to have_content "エラーが発生しました"
+    it "fails to login" do
+      click_button "ログイン"
+      expect(page).to have_content "エラー"
     end
   end
 end
